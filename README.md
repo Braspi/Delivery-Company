770704a1e396e570057e84f01e6df1851271a191
wiktor <mcserw4@wp.pl>
2023-12-07 12:07:39 +0100

    setup router

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Delivery-Company
+
+D:\Xampp\php\php.exe -S localhost:8080 or php -S localhost:8080
--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php
+include 'router.php';
+
+get('/login', view("login"));
+get('/404', view('error'));
+
+function view($name) {
+    return "/views/{$name}.view.php";
+}
