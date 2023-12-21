bacce2570707bcac1c84d34d2776e60e41d07fbc
Axon037 <axon.0371@gmail.com>
2023-12-21 07:26:49 +0100

    test

--- views/dashboard/index.view.php
+++ views/dashboard/index.view.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Document1</title>
 </head>
 <body class="bg-[#F9F9F9]">
     <div class="w-[300px] bg-[#dfdfdf] h-screen border text-[#535353]">
