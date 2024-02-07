2a302583ba25f70a63f1af3d964bb12efad0713c
Axon037 <axon.0371@gmail.com>
2024-02-07 22:00:33 +0100

    jednak nie wszystkie usunalem sory

--- src/views/dashboard/status.view.php
+++ src/views/dashboard/status.view.php
@@ -7,13 +7,13 @@
         <ul class="mb-4 items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-500 rounded-lg sm:flex">
             <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r">
                 <div class="flex items-center ps-3">
-                    <input id="radio1" type="radio" value="paid" name="payment_status" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                    <input id="radio1" type="radio" value="paid" name="payment_status" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
                     <label for="radio1" class="w-full py-3 ms-2 text-sm font-medium text-gray-900">zapłacone</label>
                 </div>
             </li>
             <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r">
                 <div class="flex items-center ps-3">
-                    <input id="radio2" type="radio" value="new_payment" name="payment_status" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                    <input id="radio2" type="radio" value="new_payment" name="payment_status" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
                     <label for="radio2" class="w-full py-3 ms-2 text-sm font-medium text-gray-900">nowa płatność</label>
                 </div>
             </li>
