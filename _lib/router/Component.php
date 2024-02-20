17106ca76a336413cb2b81359a8feaf491383380
Wiktor <mcserw4@wp.pl>
2024-02-20 22:39:32 +0100

    removed old components

--- _lib/router/Component.php
+++ _lib/router/Component.php
@@ -1,18 +1,6 @@
 <?php
 namespace _lib\router;
 
-function component(string $name, array $params = array()): void {
-    extract($params);
-    include root_path."/components/$name.component.php";
-}
-
-function _t(string $value): void {
-    echo $value;
-}
-function _o(mixed $value): void {
-    print_r($value);
-}
-
 abstract class Component {
     abstract function path(): string;
     abstract function params(): array;
